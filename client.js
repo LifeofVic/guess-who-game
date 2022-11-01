ab8877891e26293b12fa317bb1772affcc34e5f2
Victor Llapa <victorllapa@gmail.com>
2022-11-01 00:00:16 -0500

    added the ability to iterate throught the array of the array of people and add each one using the property value provided

--- client.js
+++ client.js
@@ -2,6 +2,8 @@ console.log('Here are all the available people:', people);
 
 $(document).ready(onReady);
 
+let randomPersonSelected = '';
+
 
 function onReady() {
 	console.log('We are in Jquery! 🙏');
@@ -9,7 +11,20 @@ function onReady() {
 	randomPerson(); //checks if there is a random name console.log at each page refreshed. 
 }
 
-let randomPersonSelected;
+
+function render() {
+	for (person of people) {
+		$('.workArea').append(`
+		<div id="${person.name}">
+		
+		<img src="https://github.com/${person.githubUsername}.png?size=250" atl= "Profile picture of ${person.name}">
+		
+		</div>
+		`)
+	}
+
+
+}
 
 function randomPerson(min = 0, max = 6) {
 	let index = Math.floor(Math.random() * (1 + max - min) + min);
@@ -17,6 +32,6 @@ function randomPerson(min = 0, max = 6) {
 	randomPersonSelected = people[index].name;
 
 	console.log('The name of the selectedRandomPerson is: ', randomPersonSelected);
-
-
 }
+
+
